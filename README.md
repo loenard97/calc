15171ccb8d025a8989910d5fce52bb18df6bf059
Dennis Lönard <dennis.loenard97@gmx.de>
2023-06-05 15:08:05 +0200

    added constants and single operators

--- README.md
+++ README.md
@@ -10,7 +10,62 @@ A cli calculator using reverse polish notation
 
 
 ## ▶️ Usage
+In [reverse Polish notation](https://en.wikipedia.org/wiki/Reverse_Polish_notation) operators are appended after their operands, eliminating the need for brackets.
 ```sh
 $ calc 3 2 +
 5
+```
+
+Accepts command line arguments, as well as stdin:
+```sh
+$ echo "3 2 +" | calc
+5
+```
+
+
+### Supported operators
+> **Note**
+> The naming of all operators is case insensitive
+
+Constants:
+ - `pi` Archimedes’ constant π
+ - `e` Euler's constant e
+
+Operators that take one input value:
+ - `ln` Loagaritm base e
+ - `log2` Logarithm base 2
+ - `log10` Logarithm base 10
+ - `sin` Sine
+ - `cos` Cosine
+ - `tan` Tangent
+
+Operators that take two input values:
+ - `+` Addition
+ - `-` Subtraction
+ - `.` Multiplication (Note that `.` is used for multiplication, because `*` represents a wildcard in bash)
+ - `/` Division
+ - `log` Logarithm with given base
+
+
+## ⚠️ Invalid expressions
+Invalid tokens are ignored for computation, but give a warning.
+```sh
+$ calc 3 __ 7 + ?? 6 /
+Warning: expression contains 2 invalid tokens
+ │ calc 3 __ 7 + ?? 6 /
+ └────────^^─────^^────
+```
+
+An expression can not be computed if an operator does not have enough values on the stack to complete its operation...
+```sh
+$ calc 3 +
+Error: not enough values on stack to apply operator
+ │ calc 3 +
+ └────────^
+```
+
+...or if at the end of the computation more than one value remains on the stack.
+```sh
+$ calc 3 5 7 +
+Error: stack contains more than one value after applying all operators
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,47 @@
 use std::env;
 use std::process;
+use std::io::BufRead;
+use std::f32::consts;
 
 use colorama::Colored;
 
+/// Math Constant
+/// 
+/// Is always evaluated to f32 without any input
 #[derive(Debug, PartialEq)]
-enum Operator {
+enum Constant {
+    Pi,
+    E,
+}
+
+/// Operators that consume one Stack value
+#[derive(Debug, PartialEq)]
+enum SingleOperator {
+    Ln,
+    Log2,
+    Log10,
+    Sin,
+    Cos,
+    Tan,
+}
+
+/// Operators that consume two Stack values
+#[derive(Debug, PartialEq)]
+enum DoubleOperator {
     Plus,
     Minus,
     Multiply,
     Divide,
+    Log,
 }
 
+/// Calculator Token
 #[derive(Debug, PartialEq)]
 enum Token {
     Number(f32),
-    Operator(Operator),
+    Constant(Constant),
+    SingleOperator(SingleOperator),
+    DoubleOperator(DoubleOperator),
     Invalid,
 }
 
@@ -22,10 +49,22 @@ impl Token {
     /// Create new Token from str
     fn from_str(token: &str) -> Self {
         match token {
-            "+" => Token::Operator(Operator::Plus),
-            "-" => Token::Operator(Operator::Minus),
-            "." => Token::Operator(Operator::Multiply),
-            "/" => Token::Operator(Operator::Divide),
+            "pi" => Token::Constant(Constant::Pi),
+            "e" => Token::Constant(Constant::E),
+
+            "ln" => Token::SingleOperator(SingleOperator::Ln),
+            "log2" => Token::SingleOperator(SingleOperator::Log2),
+            "log10" => Token::SingleOperator(SingleOperator::Log10),
+            "sin" => Token::SingleOperator(SingleOperator::Sin),
+            "cos" => Token::SingleOperator(SingleOperator::Cos),
+            "tan" => Token::SingleOperator(SingleOperator::Tan),
+
+            "+" => Token::DoubleOperator(DoubleOperator::Plus),
+            "-" => Token::DoubleOperator(DoubleOperator::Minus),
+            "." => Token::DoubleOperator(DoubleOperator::Multiply),
+            "/" => Token::DoubleOperator(DoubleOperator::Divide),
+            "log" => Token::DoubleOperator(DoubleOperator::Log),
+
             _ => {
                 match token.parse::<f32>() {
                     Ok(val) => Token::Number(val),
@@ -36,6 +75,7 @@ impl Token {
     }
 }
 
+/// Stack of f32 values
 #[derive(Debug)]
 struct Stack {
     stack: Vec<f32>,
@@ -48,6 +88,7 @@ impl Stack {
     }
 
     /// Push Token to stack
+    /// 
     /// Token::Invalid's are silently ignored.
     ///
     /// # Errors:
@@ -55,29 +96,64 @@ impl Stack {
     fn push(&mut self, token: &Token) -> Result<(), Box<dyn std::error::Error>> {
         match token {
             Token::Number(number) => self.stack.push(*number),
-            Token::Operator(operator) => self._apply_operator(operator)?,
+            Token::Constant(constant) => self._apply_constant(constant),
+            Token::SingleOperator(operator) => self._apply_single_operator(operator)?,
+            Token::DoubleOperator(operator) => self._apply_double_operator(operator)?,
             Token::Invalid => { },
         };
 
         Ok(())
     }
 
-    /// Apply Operator to current stack
+    /// Apply Constant to current stack
+    fn _apply_constant(&mut self, constant: &Constant) {
+        self.stack.push(
+            match constant {
+                Constant::Pi => consts::PI,
+                Constant::E => consts::E,
+            }
+        )
+    }
+
+    /// Apply SingleOperator to current stack
     ///
-    /// Errors:
+    /// # Errors:
     /// Errors when Stack does not contain enough values for the given Operator.
-    fn _apply_operator(&mut self, operator: &Operator) -> Result<(), Box<dyn std::error::Error>> {
+    fn _apply_single_operator(&mut self, operator: &SingleOperator) -> Result<(), Box<dyn std::error::Error>> {
+        let val_1 = self.stack.pop().ok_or("empty stack")?;
+
+        self.stack.push(
+            match operator {
+                SingleOperator::Ln => val_1.ln(),
+                SingleOperator::Log2 => val_1.log2(),
+                SingleOperator::Log10 => val_1.log10(),
+                SingleOperator::Sin => val_1.sin(),
+                SingleOperator::Cos => val_1.cos(),
+                SingleOperator::Tan => val_1.tan(),
+            }
+        );
+
+        Ok(())
+    }
+
+    /// Apply DoubleOperator to current stack
+    ///
+    /// # Errors:
+    /// Errors when Stack does not contain enough values for the given Operator.
+    fn _apply_double_operator(&mut self, operator: &DoubleOperator) -> Result<(), Box<dyn std::error::Error>> {
         let val_2 = self.stack.pop().ok_or("empty stack")?;
         let val_1 = self.stack.pop().ok_or("empty stack")?;
 
-        let val_ret = match operator {
-            Operator::Plus => val_1 + val_2,
-            Operator::Minus => val_1 - val_2,
-            Operator::Multiply => val_1 * val_2,
-            Operator::Divide => val_1 / val_2,
-        };
+        self.stack.push(
+            match operator {
+                DoubleOperator::Plus => val_1 + val_2,
+                DoubleOperator::Minus => val_1 - val_2,
+                DoubleOperator::Multiply => val_1 * val_2,
+                DoubleOperator::Divide => val_1 / val_2,
+                DoubleOperator::Log => val_1.log(val_2),
+            }
+        );
 
-        self.stack.push(val_ret);
         Ok(())
     }
 
@@ -91,30 +167,85 @@ impl Stack {
     }
 }
 
-/// Print msg to stderr, then
-/// exit process with code 1.
-fn process_exit(msg: &str) {
-    eprintln!("{}: {}", String::from("Application error").color("red").style("bold"), msg);
-    process::exit(1);
-}
-
 fn main() {
+    /* get input either from stdin or env args */
     let args: Vec<String> = env::args().collect();
+    let input: Vec<String> = if atty::is(atty::Stream::Stdin) {
+        let input = args.clone();
+        if input.len() == 1 {
+            eprintln!(
+                "{}: no expression given\n\
+                Usage: give a math expression in reverse Polish notation to calculate\n\
+                $ calc 3 5 +\n8", 
+                String::from("Error").color("red").style("bold")
+            );
+            process::exit(1);
+        };
+        input
+    } else {
+        let mut buffer = String::from(
+            args.iter().next().unwrap_or(&"calc".to_string())
+        );
+        buffer.push(' ');
+        _ = std::io::stdin()
+            .lock()
+            .read_line(&mut buffer);
+        buffer.split(' ')
+            .map(|s| { s.trim_end().to_string() })
+            .collect()
+    };
 
-    let tokens: Vec<_> = args.clone().iter()
-        .map(|op| { Token::from_str(op) })
+    /* parse input into Tokens */
+    let mut invalid = vec![];
+    let mut args_clone = input.clone();
+    args_clone.remove(0);
+    let tokens: Vec<_> = args_clone.iter()
+        .map(|op| { Token::from_str(&op.to_lowercase() ) })
         .collect();
 
+    for (i, tok) in tokens.iter().enumerate() {
+        if *tok == Token::Invalid {
+            invalid.push(i);
+        }
+    }
+
+    /* print warning on invalid tokens */
+    if invalid.len() > 0 {
+        eprintln!("{}: expression contains {} invalid token{}", 
+            String::from("Warning").color("yellow").style("bold"), 
+            invalid.len(), 
+            if invalid.len() == 1 { '\0' } else { 's' }
+        );
+        
+        let mut sargs = String::new();
+        let mut sline = String::new();
+        for (i, arg) in input.iter().enumerate() {
+            if i > 0 && invalid.contains(&(i-1)) {
+                let c = "^".repeat(arg.len()).color("yellow");
+                let arg_colored = String::from(arg).color("yellow");
+                sline.push_str(&c);
+                sline.push('─');
+                sargs.push_str(&format!("{} ", arg_colored));
+            } else {
+                let c = "─".repeat(arg.len() + 1);
+                sline.push_str(&c);
+                sargs.push_str(&format!("{} ", arg));
+            }
+        }
+        eprintln!(" │ {}\n └─{}", sargs, sline);
+    }
+
+    /* push tokens onto stack */
     let mut stack = Stack::new();
     for (i, tok) in tokens.iter().enumerate() {
         if stack.push(tok).is_err() {
             let mut sargs = String::new();
             let mut sline = String::new();
 
-            for (j, arg) in args.iter().enumerate() {
-                if i == j {
-                    let c = "^".repeat(arg.len());
-                    let arg_colored = String::from(arg).color("green");
+            for (j, arg) in input.iter().enumerate() {
+                if i + 1 == j {
+                    let c = "^".repeat(arg.len()).color("red");
+                    let arg_colored = String::from(arg).color("red");
                     sline.push_str(&c);
                     sargs.push_str(&format!("{} ", arg_colored));
                 } else {
@@ -122,15 +253,26 @@ fn main() {
                     sline.push_str(&c);
                     sargs.push_str(&format!("{} ", arg));
                 }
-                
             }
 
-            process_exit(&format!("not enough values on stack to apply operator\n{}\n{}", sargs, sline));
+            eprintln!(
+                "{}: not enough values on stack to apply operator\n │ {}\n └─{}", 
+                String::from("Error").color("red").style("bold"),
+                sargs, sline
+            );
+            process::exit(1);
         };
     }
 
+    /* print final value */
     match stack.value() {
         Some(val) => println!("{}", val),
-        None => process_exit("stack contains more than one value after applying all operators\nAre you missing an operator at the end?"),
+        None => {
+            eprintln!(
+                "{}: stack contains more than one value after applying all operators", 
+                String::from("Error").color("red").style("bold")
+            );
+            process::exit(1);
+        },
     };
 }
